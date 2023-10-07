500e548b23bb39043ece789a944eab86f01258e7
Ian Sills <iansills04@gmail.com>
2023-10-07 01:26:06 -0500

    updated html,js,css

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
-fetch("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=93f82ae58c45de5e77c345bebf997588")
-.then(data => data.json())
-.then(weatherData => console.log(weatherData) );
+document.getElementById('container')
 
 
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat=10&lon=20&appid=de7a2c93ef7ff607a0e6abf6ee0aa990&")
+.then(data => data)
+.then(pm => console.log(pm));
+
+
+function constructVisualizerPayload() {
+    var res = pm.response.json();
+    
+    return {response: res};
+}
+
+pm.visualizer.set(template, constructVisualizerPayload());
